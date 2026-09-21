4cd543a294a9de2b60a95391ee4334c5a710fd11
Guilherme Walter <gui.walter99@hotmail.com>
2026-09-21 20:33:36 -0300

    adiciona aulas sobre for e listas

--- aulas/estruturas-de-repeticao.py
+++ aulas/estruturas-de-repeticao.py
@@ -1,6 +1,49 @@
-contador = 1
+# while
+# contador = 1
 
-while contador <= 5:
-    print(f"contador {contador}")
+# while contador <= 10:
+#     if contador == 3:
+#         contador += 1
+#         continue
 
-    contador += 1
+#     if contador == 7:
+#         break
+
+#     print(f"contador {contador}")
+#     contador += 1
+
+
+# for
+# print("usando somente numero final")
+# for numero in range(5):
+#     print(f"numero {numero}")
+
+# print("usando numero inicial e numero final")
+# for numero in range(1, 5):
+#     print(f"numero {numero}")
+
+# print("usando numero inicial, final e incremental")
+# sequencia = range(0, 11, 2)
+# for numero in sequencia:
+#     print(f"numero {numero}")
+
+# print("usando numero inicial, final e incremental - contador negativo")
+# sequencia = range(10, -1, -1)
+# for numero in sequencia:
+#     print(f"numero {numero}")
+
+for letra in "Guilherme":
+    print(f"letra {letra}")
+
+frutas = ["Maça", "Banana", "Mamão"]
+
+for fruta in frutas:
+    print(f"fruta {fruta}")
+
+print("usando enumerate")
+for indice, fruta in enumerate(frutas):
+    print(f"indice {indice}: {fruta}")
+
+print("usando enumerate e start")
+for indice, fruta in enumerate(frutas, start=1):
+    print(f"indice {indice}: {fruta}")
---
+++ aulas/listas.py
@@ -0,0 +1,21 @@
+frutas = ["Maça", "Banana", "Mamão", "Uva"]
+
+numeros = [1, 3, 5, 10]
+
+booleanos = [True, False, True]
+
+dados = ["Guilherme", 27, True, "Programador"]
+
+for dado in dados:
+    print(f"dado {dado}")
+
+alunos = []
+
+alunos.append("Guilherme")
+alunos.append("Victor")
+alunos.append("Pedro")
+
+print(f"alunos[0] {alunos[0]}")
+print(f"alunos[1] {alunos[1]}")
+print(f"alunos[1] {alunos[-1]}")
+
